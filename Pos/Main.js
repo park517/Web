62371a3063a940c4ebe47e8420f2b83fd8361581
park517 <75284368+park517@users.noreply.github.com>
2021-02-22 10:14:36 +0900

    Update Main.js

--- Pos/Main.js
+++ Pos/Main.js
@@ -1,3 +1,13 @@
+
+temp = decodeURI(location.href).split("?"); 
+console.log(temp);
+
+
+if(temp[1] !=null){
+    print_menu();
+}
+
+
 function go_menu(table_id){
 
     content =document.getElementById(table_id).innerText; 
@@ -6,5 +16,11 @@ function go_menu(table_id){
 
 
 
-    
+
+}
+
+function print_menu(){
+    console.log(temp[0]);
+    console.log(temp[1] +"호출 완료되었습니다");
+    document.getElementById("table1_text").innerText= temp[1];
 }
