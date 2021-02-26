b7a8c779d3c88b11bcd66800e36ee4fbbdb46db9
park517 <75284368+park517@users.noreply.github.com>
2021-02-26 14:35:54 +0900

    Update Random1.js

--- Random_game/Random1.js
+++ Random_game/Random1.js
@@ -4,41 +4,54 @@
   const hint = document.querySelector(".hint");
   const guessField = document.querySelector(".guessField");
   const submitButton = document.querySelector(".submitButton");
+  const add = document.querySelector(".add");
   let count =1;
   let resetButton;
 
 
   function checkGuess() {
     let userGuess =Number(guessField.value);
-    if( count ===1){
-      inputNumber.textContent='Previous : ';
+    if(userGuess ===0){
+      hint.textContent='0을 선택한 당신.. 설명도 안읽고.. 다시 해주세요!';
     }
+    else{
 
-    inputNumber.textContent += userGuess+ ' ';
+      
+      if( count ===1){
+        inputNumber.textContent='Previous : ';
+      }
 
-    if(userGuess === randomNumber){
-      result.textContent ="축하드립니다 정답을 맞추셨습니다.";
-      result.style.backgroundColor ='green';
-      hint.textContent = '';
-      setGameOver();
-    } else if(count ===10){
-      result.textContent ="게임이 종료되었습니다."
-      result.style.backgroundColor ='red';
-      hint.textContent = '';
-      setGameOver();
-    } else{
-      result.textContent ='틀리셨습니다 다시 입력해주세요';
-      result.style.backgroundColor ='red';
-      if(randomNumber > userGuess){
-        hint.textContent = '입력 하신 숫자가 너무 작습니다';
+      inputNumber.textContent += userGuess+ ' ';
+
+
+
+      if(userGuess === randomNumber){
+        result.textContent ="축하드립니다 정답을 맞추셨습니다.";
+        result.style.backgroundColor ='green';
+        hint.textContent = '';
+        setGameOver();
+
+      } else if(count ===10){
+        result.textContent ="게임이 종료되었습니다."
+        result.style.backgroundColor ='red';
+        hint.textContent = '';
+        setGameOver();
+      } else{
+        result.textContent ='틀리셨습니다 다시 입력해주세요';
+        result.style.backgroundColor ='red';
+        if(randomNumber > userGuess){
+          hint.textContent = '입력 하신 숫자가 너무 작습니다';
+        }
+        else if(randomNumber < userGuess){
+          hint.textContent = '입력하신 숫자가 너무 큽니다.';
+        }
+
       }
-      else if(randomNumber < userGuess){
-        hint.textContent = '입력하신 숫자가 너무 큽니다.';
-      }
-    }
+    
     count++;
     guessField.value ='';
     guessField.focus();
+    }
 
   }
 
@@ -64,6 +77,5 @@
   submitButton.disabled = false;
   guessField.value ='';
   guessField.focus();
-  inputNumber.style.backgroundColor = 'white';
   randomNumber = Math.floor(Math.random()*100)+1;
 }
