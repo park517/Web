cf0fc3720e16b5df9810ec9a738de28a124633b4
park517 <park51777@naver.com>
2021-03-04 20:17:28 +0900

    확인

---
+++ Website/history.js
@@ -0,0 +1,13 @@
+// 어플 상에서 버튼으로 메뉴 토글
+const toggleBtn = document.querySelector('.navbar_toogleBtn');
+const menu   = document.querySelector('.navbar_menu');
+
+toggleBtn.addEventListener('click', ()=>{
+    menu.classList.toggle('active');
+});
+
+
+
+
+
+
